50490770ff0cfeb91ee90f3e94ddca49fa72e695
linhpksw <linhpksw@gmail.com>
2024-02-25 16:16:40 +0700

    fix connection

--- src/main/java/tech/unimart/unimart/context/DBContext.java
+++ src/main/java/tech/unimart/unimart/context/DBContext.java
@@ -13,14 +13,15 @@ public class DBContext {
     public DBContext() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // MySQL JDBC Driver
-//            String user = "root"; // MySQL username
-//            String pass = "Bmcdc54@"; // MySQL password
-//            String url = "jdbc:mysql://localhost:3306/unimartDB"; // MySQL URL and database name
-//            String url = "jdbc:mysql://db:3306/unimartDB"; // MySQL URL and database name for Docker
+            // MySQL username
+            String user = "root";
+            // MySQL password
+            String pass = "Bmcdc54@";
+            // For connecting to the database on the local machine
+            //String url = "jdbc:mysql://localhost:3306/unimartDB";
+            // For connecting to the database on the docker container
+            String url = "jdbc:mysql://db:3306/unimartDB";
 
-            String user = System.getenv("DB_USER");
-            String pass = System.getenv("DB_PASSWORD");
-            String url = "jdbc:mysql://" + System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME");
             connection = DriverManager.getConnection(url, user, pass);
 
         } catch (ClassNotFoundException | SQLException ex) {
